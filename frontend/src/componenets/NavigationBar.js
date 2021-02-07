026265c9605b2e7afc7195b6bf4dc37d5e721a6c
parth-27 <parth1020patel@gmail.com>
2021-02-07 09:00:07 +0530

    still not responsive

--- frontend/src/componenets/NavigationBar.js
+++ frontend/src/componenets/NavigationBar.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
+import { Icon } from 'semantic-ui-react';
+
 export default class NavigationBar extends Component {
     render() {
         return (
             <>
-                <Navbar bg="dark" variant="dark">
+                <Navbar bg="dark" variant="dark" expand="md">
                     <Navbar.Brand href="/">Swap Shop</Navbar.Brand>
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
@@ -15,7 +17,8 @@ export default class NavigationBar extends Component {
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-3" />
-                        {/* <Button variant="outline-info" style={{marginRight:'10px'}}>Search</Button> */}
+                        <Icon name="search" color="blue" size="large" style={{ marginRight: '10px' }}/>
+                        {/* <Button variant="outline-info" style={{ marginRight: '10px' }}></Button> */}
                         <Button variant="outline-info" style={{ marginRight: '10px' }} href = "/login" >Login</Button>
                         <Button variant="outline-info" style={{marginRight:'10px'}} href = "/register" >Sign up</Button>
                     </Form>
