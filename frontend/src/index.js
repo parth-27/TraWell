2378cc486a87bb59f32ed1735e24b89c89256a58
parth-27 <parth1020patel@gmail.com>
2021-02-11 19:36:56 +0530

    initialize

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-import "semantic-ui-css/semantic.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
