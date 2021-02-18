f9e8f80f99629335e8d278581b21a8b5a95d6c72
parth-27 <parth1020patel@gmail.com>
2021-02-18 16:01:44 +0530

    authenticate and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import Navbar from "./components/components/Navbar/Navbar";
+import { Authenticate } from "./components/Auth/index";
 
 function App() {
 	return (
@@ -12,6 +13,8 @@ function App() {
 				<Navbar />
 				<Switch>
 					<Route path='/' exact component={Home} />
+					<Route path='/login' exact component={Authenticate} />
+					{/* <Route path='/sign-up' exact component={Signup} /> */}
 				</Switch>
 			</Router>
 		</>
