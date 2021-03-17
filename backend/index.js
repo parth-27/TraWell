91121c54d5f1795e2e8e9d5efbd0a3c82780295c
Shreyansh Shah <shreyansh_shah@yahoo.com>
2021-03-17 21:37:18 +0530

    backend_1

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+// const cors = require('cors')
+// require('dotenv').config();
+const app = express()
+const port = 8000;
+const db = require('./config/mongoose');
+
+// app.use(cors());
+app.use(express.json());
+
+app.use('/',require('./routes'))
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the server ${err}`);
+    }else{
+        console.log(`Server is up and running on port ${port}`);
+    } 
+})
