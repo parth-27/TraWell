ce8efb3e82eb135b5347b7562a0422c15b21b3ea
Shreyansh Shah <shreyansh_shah@yahoo.com>
2021-03-18 10:45:07 +0530

    commit

---
+++ server/config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/TraWell',{useNewUrlParser:true,useUnifiedTopology:true});
+
+const db = mongoose.connection;
+
+db.on('error',console.log.bind(console,"Error connecting db"));
+db.once('open',function(){
+    console.log(`Connected to db`);
+});
+
+module.exports = db;
