d680b2124bf6a9b85b861a35f1119387ee466e10
parth-27 <parth1020patel@gmail.com>
2021-03-28 21:36:59 +0530

    setting global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,42 @@
+import { useReducer } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./components/pages/Home/Home";
 import Navbar from "./components/components/Navbar/Navbar";
 import { Authenticate } from "./components/pages/Auth/index.js";
 import Display from "./components/pages/Display/Display";
 import { ForgotPassword } from './components/pages/Auth/ForgotPassword';
-import {ConfirmOTP} from './components/pages/Auth/ConfirmOTP';
+import { ConfirmOTP } from './components/pages/Auth/ConfirmOTP';
 import { NewPassword } from './components/pages/Auth/NewPassword';
 
+import { userReducer, initialUserState } from './components/reducer';
+import { userContext } from './components/context';
+
 function App() {
+
+	const [user, userDispatch] = useReducer(userReducer, initialUserState);
+
 	return (
 		<>
-			<Router>
-				<Navbar />
-				<Switch>
-					<Route path='/' exact render = {()=> <Home/>} />
-					<Route path='/user/signup' exact render = {(props) => <Authenticate {...props} signup={true} />}/>
-					<Route path='/user/signin' exact render = {()=> <Authenticate /> }/>
-					<Route path='/rent' exact render = {() => <Display />} />
-					<Route path='/user/confirmOTP' exact render = {() => <ConfirmOTP />} />
-					<Route path='/forgotPassword' exact render = {() => <ForgotPassword />} />
-					<Route path='/resetPassword' exact render = {() => <NewPassword />} />
-				</Switch>
-			</Router>
+			<userContext.Provider value={{
+				currentUser: user,
+				userDispatch: userDispatch
+			}}>
+				<Router>
+					<Navbar />
+					<Switch>
+						<Route path='/' exact render={() => <Home />} />
+						<Route path='/user/signup' exact render={(props) => <Authenticate {...props} signup={true} />} />
+						<Route path='/user/signin' exact render={() => <Authenticate />} />
+						<Route path='/rent' exact render={() => <Display />} />
+						<Route path='/user/confirmOTP' exact render={() => <ConfirmOTP />} />
+						<Route path='/forgotPassword' exact render={() => <ForgotPassword />} />
+						<Route path='/resetPassword' exact render={() => <NewPassword />} />
+					</Switch>
+				</Router>
+			</userContext.Provider>
 		</>
 	);
 }
