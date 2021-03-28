ff08acbc66a0f10be7cbb240ff9d32963065fca3
parth-27 <parth1020patel@gmail.com>
2021-03-28 21:38:26 +0530

    user reducer

---
+++ client/src/components/reducer/index.js
@@ -0,0 +1,21 @@
+export const initialUserState = {
+    userEmail: "",
+    token:"",
+}
+
+export const userReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                userEmail:action.payload.email
+            }
+        case 'CLEAR_USER':
+            return {
+                ...state,
+                userEmail:"",
+            }
+        default:
+            return state
+    }
+}
