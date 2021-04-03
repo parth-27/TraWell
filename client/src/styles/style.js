f699b249d24b9948d30cb5a0d3e413d02dc590e3
parth-27 <parth1020patel@gmail.com>
2021-04-03 11:19:50 +0530

    styles

---
+++ client/src/styles/style.js
@@ -0,0 +1,97 @@
+import styled from "styled-components";
+
+export const BoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+export const FormContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 0px 2.5px rgba(15, 15, 15, 0.19);
+`;
+
+export const MutedLink = styled.a`
+  color: rgba(170, 170, 170, 1);
+  font-size: 15px;
+  font-weight: 500;
+  margin: 10px 0;
+  text-decoration: none;
+`;
+
+export const BoldLink = styled.a`
+  color: #5963c3;
+  font-weight: 600;
+  font-size: 16px;
+  text-decoration: none;
+  margin: 0 3px;
+`;
+
+export const Input = styled.input`
+  margin-bottom:10px;
+  width: 100%;
+  height: 42px;
+  outline: none;
+  border: 1px solid rgba(200, 200, 200, 0.03);
+  padding: 0 10px;
+  transition: all, 200ms ease-in-out;
+  box-sizing: border-box;
+  border-bottom: 1.4px solid transparent;
+  &::placeholder {
+    color: rgba(170, 170, 170, 1);
+  }
+  &:not(:last-of-type) {
+    border-bottom: 1.4px solid rgba(200, 200, 200, 0.4);
+  }
+  &:focus {
+    outline: none;
+    box-shadow: 0px 0px 2px rgba(200, 200, 200, 1);
+    border-bottom: 2px solid #5963c3;
+  }
+  box-shadow : 0px 0px 2.5px rgb(185, 185, 185);
+`;
+
+export const SubmitButton = styled.button`
+  background-color: transparent;
+  padding: 11px 40%;
+  width: 100%;
+  color: #151515;
+  font-size: 20px;
+  font-weight: 600;
+  border: 1px solid #151515;
+  border-radius: 2px;
+  cursor: pointer;
+  transition: all, 240ms ease-in-out;
+  background: #fff; /* fallback for old browsers */
+  
+  
+  /* Chrome 10-25, Safari 5.1-6 */
+  ${'' /* background: -webkit-linear-gradient(
+    to right,
+    #2ebf91,
+    #8360c3
+  );  */}
+  
+  
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  ${'' /* background: linear-gradient(
+    to right,
+    #2ebf91,
+    #8360c3
+  );  */}
+  
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    filter: brightness(1.03);
+	  transition: all 0.3s ease-out;
+  	background: #151515;
+  	color: #fff;
+  	transition: 250ms;
+  }
+`;
